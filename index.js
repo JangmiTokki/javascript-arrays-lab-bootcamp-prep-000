fd5cac9400d4e2343524e6fab60cc80191850490
communal-principle-3181 <aellis19@outlook.com>
2019-05-20 06:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+  return kittens;
+}
+
+function appendKitten(name)
+{
+  kittens = [name, ...kittens];
+  return kittens;
+}
+
+function prependKitten(name)
+{
+  
+  kittens = [...kittens, name];
+  return kittens;
+}
